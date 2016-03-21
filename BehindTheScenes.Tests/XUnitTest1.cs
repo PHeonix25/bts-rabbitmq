fb8d01c4a50e9f57769f88612ad4ce29e6643cdd
Pat Hermens <p.hermens@coolblue.nl>
2016-03-21 10:00:10 +0100

    Add XUnit to the Testing project

---
+++ BehindTheScenes.Tests/XUnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+
+namespace BehindTheScenes.Tests
+{
+    public class XUnitTest1
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(true, true);
+        }
+
+        [Theory]
+        [InlineData(true)]
+        public void PassingTestWithInput(bool input)
+        {
+            Assert.Equal(true, input);
+        }
+    }
+}
