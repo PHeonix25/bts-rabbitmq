3fe224a5576e0f8a66f9dc663a817178c9244f3b
Pat Hermens <p.hermens@coolblue.nl>
2016-04-20 17:06:45 +0200

    Updated to remove the shared TimeSpan constant

--- BehindTheScenes.Core/Polly/Policies/GenericRetryPolicy.cs
+++ BehindTheScenes.Core/Polly/Policies/GenericRetryPolicy.cs
@@ -7,12 +7,19 @@ namespace BehindTheScenes.Core.Polly.Policies
 {
     public class GenericRetryPolicy : IRetryPolicy
     {
+        private readonly TimeSpan[] _genericRetryDelays =
+        {
+            TimeSpan.FromSeconds(10),
+            TimeSpan.FromSeconds(10),
+            TimeSpan.FromSeconds(10)
+        };
+
         private readonly RetryPolicy _policy;
 
         public GenericRetryPolicy()
         {
             _policy = Policy.Handle<Exception>()
-                            .WaitAndRetry(RetryConstants.GenericRetryDelays);
+                            .WaitAndRetry(_genericRetryDelays);
         }
 
         public void ExecuteAction(Action action) => _policy.Execute(action);
--- BehindTheScenes.Core/Polly/Policies/SpecificExceptionPolicy.cs
+++ BehindTheScenes.Core/Polly/Policies/SpecificExceptionPolicy.cs
@@ -7,13 +7,19 @@ namespace BehindTheScenes.Core.Polly.Policies
 {
     public class SpecificExceptionPolicy<TException> : IRetryPolicy where TException : Exception
     {
+        private readonly TimeSpan[] _genericRetryDelays =
+{
+            TimeSpan.FromSeconds(10),
+            TimeSpan.FromSeconds(10),
+            TimeSpan.FromSeconds(10)
+        };
         private readonly RetryPolicy _policy;
 
         public SpecificExceptionPolicy()
         {
             _policy = Policy.Handle<TException>()
                             .Or<Exception>()
-                            .WaitAndRetry(RetryConstants.GenericRetryDelays);
+                            .WaitAndRetry(_genericRetryDelays);
         }
 
         public void ExecuteAction(Action action) => _policy.Execute(action);
