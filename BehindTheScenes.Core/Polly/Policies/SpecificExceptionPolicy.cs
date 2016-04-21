ff848354921abc50ef174234936d49a28c53bfcd
Pat Hermens <p.hermens@coolblue.nl>
2016-04-21 11:55:23 +0200

    Exposed the RetryPolicy for ease of Bootstrapping

--- BehindTheScenes.Core/Polly/Policies/SpecificExceptionPolicy.cs
+++ BehindTheScenes.Core/Polly/Policies/SpecificExceptionPolicy.cs
@@ -8,22 +8,23 @@ namespace BehindTheScenes.Core.Polly.Policies
     public class SpecificExceptionPolicy<TException> : IRetryPolicy where TException : Exception
     {
         private readonly TimeSpan[] _genericRetryDelays =
-{
+        {
             TimeSpan.FromSeconds(10),
             TimeSpan.FromSeconds(10),
             TimeSpan.FromSeconds(10)
         };
-        private readonly RetryPolicy _policy;
 
         public SpecificExceptionPolicy()
         {
-            _policy = Policy.Handle<TException>()
-                            .Or<Exception>()
-                            .WaitAndRetry(_genericRetryDelays);
+            GetPolicy = Policy.Handle<TException>()
+                              .Or<Exception>()
+                              .WaitAndRetry(_genericRetryDelays);
         }
 
-        public void ExecuteAction(Action action) => _policy.Execute(action);
+        public RetryPolicy GetPolicy { get; }
 
-        public T ExecuteFunc<T>(Func<T> func) => _policy.Execute(func);
+        public void ExecuteAction(Action action) => GetPolicy.Execute(action);
+
+        public T ExecuteFunc<T>(Func<T> func) => GetPolicy.Execute(func);
     }
 }
